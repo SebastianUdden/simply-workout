ac4f2dba506ee93a00858b2fa2c558b1e77a0152
Sebastian Uddén <sebastian.udden@gmail.com>
2022-04-03 23:31:41 +0200

    bug: build issue

--- src/pages/AddExercise.tsx
+++ src/pages/AddExercise.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { exerciseNames, exerciseTypes } from "../constants/exerciseTypes";
 import { getOldWorkout, saveWorkout, uuidv4 } from "../utils";
